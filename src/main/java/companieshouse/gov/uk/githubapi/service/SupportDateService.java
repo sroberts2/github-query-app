c8bafea31daa74a03d12fd984ccb35dee2145eab
sroberts2 <sroberts2@companieshouse.gov.uk>
2023-05-31 13:33:10 +0100

    adding initial call to get spring boot lifecycle dates

---
+++ src/main/java/companieshouse/gov/uk/githubapi/service/SupportDateService.java
@@ -0,0 +1,8 @@
+package companieshouse.gov.uk.githubapi.service;
+
+import companieshouse.gov.uk.githubapi.model.SupportDate;
+
+public interface SupportDateService {
+
+    SupportDate addSupportDate(SupportDate supportDate);
+}
